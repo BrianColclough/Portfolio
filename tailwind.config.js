b3147a3028da093bc6213a9f6d1fbddae424a310
Brian Colclough <brianmcolclough@gmail.com>
2022-02-10 18:46:55 -0500

    new font for intro

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
       sans: ["Helvetica Neue", "ui-sans-serif", "system-ui"],
       serif: ["ui-serif", "Georgia"],
       mono: ["ui-monospace", "SFMono-Regular"],
-      display: ["Oswald"],
-      body: ['"Open Sans"'],
+      display: ["Manrope", "sans-serif"],
+      body: ["Open Sans"],
     },
     extend: {
       gap: {
