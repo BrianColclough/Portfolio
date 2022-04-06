ddd7f0f23e023896218708f4d915b03437e8700f
Brian Colclough <brianmcolclough@gmail.com>
2022-04-06 13:18:44 -0400

    updated to react 18

--- components/pictureViewer.jsx
+++ components/pictureViewer.jsx
@@ -1,48 +1,51 @@
-import Image from "next/image";
-import concordPic from "../assets/ConcordParkNov 12 2021-0163.jpg";
-import concordPic1 from "../assets/ConcordParkNov 12 2021-0185.jpg";
-import concordPic2 from "../assets/ConcordParkNov 12 2021-0164.jpg";
-import christmas from "../assets/Xmas-Dec 24 2021-0041.jpg";
-import boardwalk from "../assets/bwalk-Feb 01 2022-0010.jpg";
-import leaves from "../assets/FujiRecipeTesting_Nov 24 2021-0033.jpg";
-import plant from "../assets/ConcordParkNov 12 2021-0111.jpg";
-import parkingDeck from "../assets/CampusWalkDiffusion0005Nov 07 2021.jpg";
-import plant1 from "../assets/FujiRecipeTesting_Nov 24 2021-0013.jpg";
-import portrait from "../assets/ConcordParkNov 12 2021-0182.jpg";
-import spring1 from "../assets/springBreak01.jpg";
-import spring2 from "../assets/springBreak02.jpg";
-import spring3 from "../assets/springBreak03.jpg";
-import spring4 from "../assets/springBreak04.jpg";
-
 import React, { Fragment, useState } from "react";
-import Grid from "./pictures/grid";
 import Picture from "./picture";
 
 const PictureViewer = (props) => {
   return (
     <>
-      <Grid>
+      <div className="flex flex-col sm:px-8 sm:py-0 sm:flex-row sm:flex-wrap gap-8 sm:items-center sm:justify-center py-2 px-2">
         <Picture
-          image={concordPic}
+          image="/assets/ConcordParkNov 12 2021-0163.jpg"
           href={"picture of leaves in a park in corcord"}
         />
-        <Picture image={concordPic1} alt={"picture of music shop"} />
-        <Picture image={concordPic2} alt={"picture of park house"} />
-        <Picture image={christmas} alt={"picture of pit bull eating"} />
         <Picture
-          image={boardwalk}
+          image="/assets/ConcordParkNov 12 2021-0185.jpg"
+          alt={"picture of music shop"}
+        />
+        <Picture
+          image="/assets/ConcordParkNov 12 2021-0164.jpg"
+          alt={"picture of park house"}
+        />
+        <Picture
+          image="/assets/Xmas-Dec 24 2021-0041.jpg"
+          alt={"picture of pit bull eating"}
+        />
+        <Picture
+          image="/assets/bwalk-Feb 01 2022-0010.jpg"
           alt={"picture of girl walking on boardwalk"}
         />
-        <Picture image={leaves} alt={"Leaves at UNCC botanical garden"} />
-        <Picture image={plant} alt={"picture of plant in concord park"} />
-        <Picture image={parkingDeck} alt={"parking deck at uncc"} />
-        <Picture image={plant1} alt={"Picture from UnCC botanical gardens"} />
-        <Picture image={portrait} alt={"Portrait in Concord"} />
-        <Picture image={spring1} alt={"Historic house in Wilmington"} />
-        <Picture image={spring2} alt={"Historic house in Wilmington2"} />
-        <Picture image={spring3} alt={"Grass by the water"} />
-        <Picture image={spring4} alt={"Flowers in garden"} />
-      </Grid>
+        <Picture
+          image="/assets/FujiRecipeTesting_Nov 24 2021-0033.jpg"
+          alt={"Leaves at UNCC botanical garden"}
+        />
+        <Picture
+          image="/assets/ConcordParkNov 12 2021-0111.jpg"
+          alt={"picture of plant in concord park"}
+        />
+        <Picture
+          image="/assets/CampusWalkDiffusion0005Nov 07 2021.jpg"
+          alt={"parking deck at uncc"}
+        />
+        <Picture
+          image="/assets/FujiRecipeTesting_Nov 24 2021-0013.jpg"
+          alt={"Picture from UnCC botanical gardens"}
+        />
+        <Picture
+          image="/assets/ConcordParkNov 12 2021-0182.jpg"
+          alt={"Portrait in Concord"}
+        />
+      </div>
     </>
   );
 };
