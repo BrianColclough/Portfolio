1968c3aef1e1c5a041d6dfb977aebfe8189961a5
Brian Colclough <brianmcolclough@gmail.com>
2022-07-30 21:22:32 -0400

    MDX and tailwind typography working

---
+++ components/blog/centered-header.tsx
@@ -0,0 +1,6 @@
+
+export default function CenteredHeader(props) {
+  return (
+    <span className="text-center">{props.children}</span>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["1drv.ms"],
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    providerImportSource: '@mdx-js/react',
   },
-};
+})
+module.exports = withMDX({
+  // Append the default value with md extensions
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+})
