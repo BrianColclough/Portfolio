54782778fc5ed79ade0b7227409e04241ebd05b2
Brian Colclough <brianmcolclough@gmail.com>
2022-08-07 17:14:27 -0400

    looks for and automatically generates blog page and individual posts

--- components/pictures/pictureViewer.tsx
+++ components/pictures/pictureViewer.tsx
@@ -12,8 +12,12 @@ const PictureViewer = () => {
           <div className="flex flex-row flex-wrap justify-center gap-4">
             {Pictures.map((item) => (
               <React.Fragment key={item.id}>
-                <div className="basis-full h-fit w-fit flex flex-row items-center justify-center mx-2 sm:mx-0">
-                  <img src={item.path} className="rounded-lg max-h-screen" alt="{item.description}"/>
+                <div className="basis-full h-fit w-fit flex flex-row items-center justify-center">
+                  <img
+                    src={item.path}
+                    alt={item.description}
+                    className="rounded-lg max-h-screen"
+                  />
                 </div>
               </React.Fragment>
             ))}
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -4,22 +4,19 @@ import fs from "fs";
 import matter from "gray-matter";
 import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote";
-import Image from "next/image";
-import SyntaxHighlight from "../../components/blog/SyntaxHighlight";
 
 function PostPage({ frontMatter, mdxSource }) {
-  const components = { Image, SyntaxHighlight};
   return (
     <Layout>
-      <section className="flex items-center flex-col sm:leading-relaxed font-wotfard-300 pt-4 ">
+      <section className="flex  items-center flex-col sm:leading-relaxed font-wotfard-300 pt-4">
         <div
-          className="font-wotfard-300 subpixel-antialiased font-normal 
-            text-ui-text-regular justify-center sm:max-w-3xl"
+          className="font-sans subpixel-antialiased font-normal 
+            text-ui-text-regular sm:w-5/6 justify-center sm:max-w-5xl max-w-full"
         >
-          <div className="flex flex-row flex-wrap justify-center gap-4 w-full">
-            <article className="prose sm:prose-lg prose-h1:text-ui-bright-blue prose-h2:text-ui-purple sm:w-[48rem] px-2 my-2" >
+          <div className="flex flex-row flex-wrap justify-center gap-4">
+            <article className="prose sm:prose-lg prose-h1:text-ui-bright-blue prose-h2:text-ui-purple">
               <h1>{frontMatter.title}</h1>
-              <MDXRemote {...mdxSource} components={components} />
+              <MDXRemote {...mdxSource} />
             </article>
           </div>
         </div>
