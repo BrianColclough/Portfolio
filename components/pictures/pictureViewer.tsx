6b8b165751d9e9a5ae787a326d316fd44fc81f74
Brian Colclough <brianmcolclough@gmail.com>
2022-08-21 00:12:37 -0400

    better pictures on mobile and formatting

--- components/pictures/pictureViewer.tsx
+++ components/pictures/pictureViewer.tsx
@@ -1,30 +1,27 @@
 import React from "react";
-import { Pictures } from "./pictureArr"
+import { Pictures } from "./pictureArr";
 
 const PictureViewer = () => {
-    
-    return(
-        <>
-          <section className="flex  items-center flex-col sm:leading-relaxed font-wotfard-300 pt-4">
-                <div
-                    className="font-sans subpixel-antialiased font-normal
+  return (
+    <>
+      <section className="flex  items-center flex-col sm:leading-relaxed font-wotfard-300 pt-4">
+        <div
+          className="font-sans subpixel-antialiased font-normal
                     text-ui-text-regular sm:w-5/6 justify-center sm:max-w-5xl max-w-full"
-                >
-                    <div className="flex flex-row flex-wrap justify-center gap-4">
-                        {Pictures.map(item => (
-                            <React.Fragment key={item.id}>
-                                <div className="basis-full h-fit w-fit flex flex-row items-center justify-center">
-                                    <img src={item.path}
-                                        className="rounded-lg max-h-screen"
-                                    />
-                                </div>
-                            </React.Fragment>
-                        ))}
-                    </div>
+        >
+          <div className="flex flex-row flex-wrap justify-center gap-4">
+            {Pictures.map((item) => (
+              <React.Fragment key={item.id}>
+                <div className="basis-full h-fit w-fit flex flex-row items-center justify-center mx-2 sm:mx-0">
+                  <img src={item.path} className="rounded-lg max-h-screen" alt="{item.description}"/>
                 </div>
-            </section>
-        </>
-    )
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default PictureViewer;
